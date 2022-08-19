afe73af0f4ef15b548e99ebe9484dce000d78a5e
allanornel <allanornel@gmail.com>
2022-08-19 14:00:06 -0300

    fix: try to correct cors

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,11 @@ const app = express();
 export default app;
 app.use(json());
 app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(router);
 app.use(errorHandlerMiddleware);
 
